0153a855139f8e5676605edd8be17fbbda83cc33
John Asper <agentx1994@gmail.com>
2022-12-21 21:48:56 -0500

    Parses ports from URLs

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,19 @@ enum Protocol {
     Http,
 }
 
+impl Protocol {
+    pub fn get_port(&self) -> u16 {
+        match self {
+            Protocol::Http => 80,
+        }
+    }
+}
+
 #[derive(Debug, PartialEq)]
 struct ParsedUrl {
     protocol: Protocol,
     domain_name: String,
+    port: u16,
     path: String,
 }
 
@@ -24,20 +33,31 @@ impl ParsedUrl {
     fn parse(url: &str) -> Self {
         let mut parts = url.split('/');
         let protocol_str = parts.next().expect("Invalid url, unable to read protocol");
-        let _ = parts.next();
-        let domain_name = parts
-            .next()
-            .expect("Invalid url, unable to read domain name")
-            .to_string();
-        let path = parts.collect::<Vec<&str>>().join("/");
-        let path = format!("/{}", path);
         let protocol = match protocol_str {
             "http:" => Protocol::Http,
             _ => panic!("Unknown protocol {}", protocol_str),
         };
+        let _ = parts.next();
+        let (domain_name, port) = {
+            let domain_name_port = parts
+                .next()
+                .expect("Invalid url, unable to read domain name")
+                .to_string();
+            if let Some(loc) = domain_name_port.rfind(':') {
+
+                let (domain_name, port_str) = domain_name_port.split_at(loc);
+                let port_str = &port_str[1..]; // remove colon
+                (domain_name.to_string(), port_str.parse::<u16>().expect("Invalid port"))
+            } else {
+                (domain_name_port, protocol.get_port())
+            }
+        };
+        let path = parts.collect::<Vec<&str>>().join("/");
+        let path = format!("/{}", path);
         ParsedUrl {
             protocol,
             domain_name,
+            port,
             path,
         }
     }
@@ -59,7 +79,7 @@ fn fetch_url(url: &str) -> io::Result<HttpResponse> {
     let parsed_url = ParsedUrl::parse(url);
     println!("{:?}", parsed_url);
     assert!(parsed_url.protocol == Protocol::Http);
-    let mut socket = TcpStream::connect((&parsed_url.domain_name[..], 80u16))?;
+    let mut socket = TcpStream::connect((&parsed_url.domain_name[..], parsed_url.port))?;
 
     let mut request = HttpRequest::new(HttpMethod::Get, parsed_url.path, HttpVersion::Version1_1);
     request.add_header("Host", parsed_url.domain_name);
